9afb8069e8a4df4c20b16f4337146e88223d50f0
Medidali <m.mohamedidali.com>
2023-09-25 16:50:00 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
+#!/usr/bin/puyhon3
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         print()
-        return True
     except (ValueError, TypeError):
         return False
+    else:
+        return True
+
