df955742affd417ff7fc2a14d79f67ce3fafecf5
Medidali <m.mohamedidali.com>
2023-09-27 03:10:03 -0700

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"classe square"
 
 
 class Square:
+    "class with attribue size"
 
     def __init__(self, size=0):
-
+        "inisialization"
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
