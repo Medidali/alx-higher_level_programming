2ac4f779d5b1dc3a1537b1f2b6df5ecc7c0639a7
Medidali <m.mohamedidali.com>
2023-10-05 08:17:44 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""defines function to scalar divde matrix"""
+"""Divide a matrix"""
 
 
 def matrix_divided(matrix, div):
-    """divides matrix by scalar integer, rounded to two decimal places"""
-    import decimal
+    """divides all elements of a matrix"""
     error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list:
+    if (matrix == [] or matrix == [[]] or
+        type(matrix) is not list or
+            not all(type(row) is list for row in matrix)):
         raise TypeError(error_msg)
-    len_rows = []
-    row_count = 0
     for row in matrix:
-        if type(row) is not list:
-            raise TypeError(error_msg)
-        len_rows.append(len(row))
         for element in row:
             if type(element) not in [int, float]:
                 raise TypeError(error_msg)
-        row_count += 1
-    if len(set(len_rows)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    if int(div) == 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = list(map(lambda row:
-                          list(map(lambda x: round(x/div, 2), row)), matrix))
-    return new_matrix
+
+    return [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
