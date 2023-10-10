165e1b5691fa1196ec3af7a75d3a8d726f88c362
Medidali <m.mohamedidali.com>
2023-10-09 18:17:27 -0700

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"is an instance?"
+
+
+def is_same_class(obj, a_class):
+    """function that returns True if the object
+    is exactly an instance of the specified class"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
