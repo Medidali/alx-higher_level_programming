d000aa73e3d986767b2055547b79271dca047cda
Medidali <m.mohamedidali.com>
2023-11-14 11:47:01 -0800

    all tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,6 @@
 
 -- CREATETABLE
 CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
--- insertrecord
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'BOB', 14);
