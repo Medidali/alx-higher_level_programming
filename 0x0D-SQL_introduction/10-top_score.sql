8d571d5486605ba346af0755b4acad22693be716
Medidali <m.mohamedidali.com>
2023-11-14 11:27:47 -0800

    all tasks

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table
+
+-- Display
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- script that displays the number of records
+
+-- DisplayNumberRecord
+SELECT COUNT(*)
+FROM first_table
+WHERE id = 89;
