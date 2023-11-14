52d8f1e02041ff57fc4bbc4ddd49727b1650266c
Medidali <m.mohamedidali.com>
2023-11-14 11:45:34 -0800

    all tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- script that creates a table second_table
+-- script that creates a table second_tables
 
 -- CREATETABLE
 CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
